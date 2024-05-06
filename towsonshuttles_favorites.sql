5b0a0d1b452591d93568f6f7f4a0bf3a71ebe5dc
rguthier <125926329+rguthier@users.noreply.github.com>
2024-05-06 19:28:47 -0400

    Update towsonshuttles_favorites.sql with create database statement

--- towsonshuttles_favorites.sql
+++ towsonshuttles_favorites.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `favorites`
 --
 
+CREATE DATABASE IF NOT EXISTS towsonshuttles;
+USE towsonshuttles;
+
 DROP TABLE IF EXISTS `favorites`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
