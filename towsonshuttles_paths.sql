f129958ad8308e298768f10c0744a677b7182ed0
rguthier <125926329+rguthier@users.noreply.github.com>
2024-05-06 19:29:02 -0400

    Update towsonshuttles_paths.sql with create database statement

--- towsonshuttles_paths.sql
+++ towsonshuttles_paths.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `paths`
 --
 
+CREATE DATABASE IF NOT EXISTS towsonshuttles;
+USE towsonshuttles;
+
 DROP TABLE IF EXISTS `paths`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
